25d8d2bb2b22cbc844f9eeb1eb8b4d05d496ac63
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-02-21 13:01:34 -0500

    app-demo-3 starter code

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../css/App.css';
+import data from "../data/data";
+
+function App() {
+  console.log(data);
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+export default App;
